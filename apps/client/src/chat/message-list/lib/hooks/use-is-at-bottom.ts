0cfb6a6a7c00c298570bb01ab9fb9bdd49a7f568
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2024-05-12 18:31:37 -0500

    feat(client): virtualize message list

---
+++ apps/client/src/chat/message-list/lib/hooks/use-is-at-bottom.ts
@@ -0,0 +1,50 @@
+import React from "react";
+import { type FollowOutput } from "react-virtuoso";
+import * as O from "effect/Option";
+import { pipe } from "effect";
+
+type Args = {
+  scrollParent: HTMLDivElement | undefined;
+};
+type Return = {
+  isAtBottom: boolean;
+  followOutput: FollowOutput;
+};
+export function useHandleIsAtBottom({ scrollParent }: Args): Return {
+  const [isAtBottom, setIsAtBottom] = React.useState(false);
+
+  const handleIsAtBottom = React.useCallback(() => {
+    pipe(
+      O.fromNullable(scrollParent),
+      O.map((scrollParent) => {
+        const { scrollTop, clientHeight, scrollHeight } = scrollParent;
+        const threshold = 15;
+        setIsAtBottom(scrollTop + clientHeight >= scrollHeight - threshold);
+      }),
+    );
+  }, [scrollParent]);
+
+  React.useEffect(() => {
+    pipe(
+      O.fromNullable(scrollParent),
+      O.map((scrollParent) => {
+        scrollParent.addEventListener("scroll", handleIsAtBottom);
+        return () => {
+          scrollParent.removeEventListener("scroll", handleIsAtBottom);
+        };
+      }),
+      O.getOrElse(() => {
+        return () => null;
+      }),
+    );
+  }, [handleIsAtBottom, scrollParent]);
+
+  const followOutput: FollowOutput = React.useCallback(() => {
+    return isAtBottom ? "smooth" : false;
+  }, [isAtBottom]);
+
+  return {
+    isAtBottom,
+    followOutput,
+  };
+}
--- apps/client/src/chat/message-list/message-list.tsx
+++ apps/client/src/chat/message-list/message-list.tsx
@@ -1,11 +1,16 @@
 import { ScrollArea } from "@/components/scroll-area";
 import React from "react";
 import { MessageBubble } from "./message-bubble";
-import { ChatMessage } from "@org/api-contract";
+import { type ChatMessage } from "@org/api-contract";
 import { client } from "@/lib/client";
+import { Virtuoso, type VirtuosoHandle } from "react-virtuoso";
+import { useHandleIsAtBottom } from "./lib/hooks/use-is-at-bottom";
 
 export const MessageList: React.FC = () => {
   const [messages, setMessages] = React.useState<ChatMessage[]>([]);
+  const virtuosoRef = React.useRef<VirtuosoHandle>(null);
+  const [scrollParent, setScrollParent] = React.useState<HTMLDivElement | undefined>();
+  const { followOutput } = useHandleIsAtBottom({ scrollParent });
 
   React.useEffect(() => {
     client.chat
@@ -14,20 +19,27 @@ export const MessageList: React.FC = () => {
       .then(setMessages);
   }, []);
 
-  React.useLayoutEffect(() => {
-    // scroll to bottom whenever messages change
-    document.getElementById("viewport-bottom")?.scrollIntoView();
-  }, [messages]);
+  const Row = React.useMemo(
+    () => (i: number, message: ChatMessage) => {
+      return <MessageBubble key={i} message={message} />;
+    },
+    [],
+  );
 
   return (
-    <ScrollArea className="relative flex-1 overflow-y-hidden px-6">
-      <ScrollArea.Viewport className="pt-4">
-        {messages.map((message, i) => (
-          <MessageBubble key={i} message={message} />
-        ))}
-
-        <div className="h-4" id="viewport-bottom" />
-      </ScrollArea.Viewport>
-    </ScrollArea>
+    <React.Fragment>
+      <ScrollArea className="relative flex-1 overflow-y-hidden px-6">
+        <ScrollArea.Viewport className="pt-4" ref={(ref) => setScrollParent(ref ?? undefined)}>
+          <Virtuoso
+            ref={virtuosoRef}
+            data={messages}
+            initialTopMostItemIndex={messages.length - 1}
+            itemContent={Row}
+            customScrollParent={scrollParent}
+            followOutput={followOutput}
+          />
+        </ScrollArea.Viewport>
+      </ScrollArea>
+    </React.Fragment>
   );
 };
