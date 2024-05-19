f20058ba00db04ac3e581093c2d668ac0ed36f2e
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2024-05-19 16:56:03 -0500

    chore(client): integrate tanstack query

--- apps/client/src/lib/client.ts
+++ apps/client/src/lib/client.ts
@@ -1,5 +1,6 @@
 import type { WsEvents, Api } from "@org/api";
 import { hc } from "hono/client";
+import { QueryClient } from "@tanstack/react-query";
 
 export const client = hc<Api>("http://localhost:3000");
 export const ws = client.ws.$ws(0);
@@ -8,3 +9,5 @@ ws.addEventListener("message", (event) => {
   const eventData: WsEvents = JSON.parse(event.data);
   console.log(eventData);
 });
+
+export const queryClient = new QueryClient();
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Chat } from "./chat/chat";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Chat />
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Chat />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
