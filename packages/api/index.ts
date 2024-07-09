fd0ff73452fcf45f903389cdcda47fe06e1b9dae
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2024-07-08 21:37:40 -0500

    feat(api): mark messages as read endpoint

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -3,12 +3,12 @@ import { createBunWebSocket } from "hono/bun";
 import { zValidator } from "@hono/zod-validator";
 import { chatMessageSchema } from "@org/api-contract";
 import { cors } from "hono/cors";
-import { WSContext } from "hono/ws";
 import { WsEvents } from "./types";
-import { Duration, Effect, Option, Random } from "effect";
+import { Duration, Effect, Random, pipe } from "effect";
 import { ChatMessagesService } from "./services/chat-messages.service";
 import { EndpointRuntime } from "./runtimes/endpoint.runtime";
 import { WebSocketBroadcastService } from "./services/websocket.service";
+import { z } from "zod";
 
 const app = new Hono();
 app.use(
@@ -20,76 +20,82 @@ app.use(
 );
 
 const { upgradeWebSocket, websocket } = createBunWebSocket();
-const activeConnections = new Set<WSContext>();
-
-function broadcastMessage(event: WsEvents) {
-  for (const ws of activeConnections) {
-    ws.send(JSON.stringify(event));
-  }
-}
 
 const router = app
   .get("/chat", (c) => {
-    return Effect.gen(function* () {
-      const chatMessagesService = yield* ChatMessagesService;
-      const allMessages = yield* chatMessagesService.getAll.pipe(
-        Effect.tap((messages) => Effect.log(`Got ${messages.length} messages`)),
-      );
+    return pipe(
+      Effect.gen(function* () {
+        const chatMessagesService = yield* ChatMessagesService;
+        const allMessages = yield* chatMessagesService.getAll.pipe(
+          Effect.tap((messages) => Effect.log(`Got ${messages.length} messages`)),
+        );
 
-      const sleepFor = yield* Random.nextRange(0.5, 1.5);
-      yield* Effect.sleep(Duration.seconds(sleepFor)).pipe(
-        Effect.tap(() => Effect.log(`Sleeping for ${sleepFor} seconds`)),
-      );
+        const sleepFor = yield* Random.nextRange(0.5, 1.5);
+        yield* Effect.sleep(Duration.seconds(sleepFor)).pipe(
+          Effect.tap(() => Effect.log(`Sleeping for ${sleepFor} seconds`)),
+        );
 
-      yield* Effect.log(`Responding to client`);
-      return c.json(allMessages);
-    }).pipe(Effect.annotateLogs({ endpoint: "getting all messages" }), EndpointRuntime.runPromise);
+        yield* Effect.log(`Responding to client`);
+        return c.json(allMessages);
+      }),
+      Effect.annotateLogs({ endpoint: "/chat", method: "GET" }),
+      EndpointRuntime.runPromise,
+    );
   })
   .post("/chat", zValidator("json", chatMessageSchema.omit({ id: true })), async (c) => {
-    return Effect.gen(function* () {
-      const chatMessagesService = yield* ChatMessagesService;
-      const wsService = yield* WebSocketBroadcastService;
+    return pipe(
+      Effect.gen(function* () {
+        const chatMessagesService = yield* ChatMessagesService;
+        const wsService = yield* WebSocketBroadcastService;
 
-      const incomingMessage = c.req.valid("json");
-      const newMessage = yield* chatMessagesService
-        .add(incomingMessage)
-        .pipe(Effect.tap((message) => Effect.log(`Added message with id ${message.id}`)));
+        const incomingMessage = c.req.valid("json");
+        const newMessage = yield* chatMessagesService
+          .add(incomingMessage)
+          .pipe(Effect.tap((message) => Effect.log(`Added message with id ${message.id}`)));
 
-      yield* wsService.broadcast({ type: "new-message", message: newMessage });
+        yield* wsService.broadcast({ type: "new-message", message: newMessage });
 
-      return c.json(newMessage);
-    }).pipe(Effect.annotateLogs({ endpoint: "adding a message" }), EndpointRuntime.runPromise);
+        return c.json(newMessage);
+      }),
+      Effect.annotateLogs({ endpoint: "/chat", method: "POST" }),
+      EndpointRuntime.runPromise,
+    );
   })
-  .patch("/chat/read", zValidator("json", chatMessageSchema.pick({ id: true, readAt: true })), (c) => {
-    return Effect.gen(function* () {
-      const chatMessagesService = yield* ChatMessagesService;
-      const wsService = yield* WebSocketBroadcastService;
+  .patch(
+    "/chat/mark-as-read",
+    zValidator("json", z.array(chatMessageSchema.pick({ id: true, readAt: true })).max(50)),
+    (c) => {
+      return pipe(
+        Effect.gen(function* () {
+          const chatMessagesService = yield* ChatMessagesService;
+          const wsService = yield* WebSocketBroadcastService;
 
-      const { id, readAt } = c.req.valid("json");
-      const message = yield* chatMessagesService.markAsRead(id, readAt);
-      if (Option.isNone(message)) {
-        const notFound = c.notFound();
-        if (notFound instanceof Promise) return yield* Effect.promise(() => notFound);
-        return notFound;
-      }
+          const messages = c.req.valid("json");
+          for (const message of messages) {
+            yield* chatMessagesService.markAsRead(message.id, message.readAt);
+          }
 
-      yield* wsService.broadcast({ type: "read-message", message: message.value });
+          yield* wsService.broadcast({ type: "read-messages", messages });
 
-      return c.json(message.value);
-    }).pipe(EndpointRuntime.runPromise);
-  })
+          return c.json(messages);
+        }),
+        Effect.annotateLogs({ endpoint: "/chat/mark-as-read", method: "PATCH" }),
+        EndpointRuntime.runPromise,
+      );
+    },
+  )
   .get(
     "/ws",
     upgradeWebSocket(() => {
       return {
         onOpen(_event, ws) {
           EndpointRuntime.runSync(
-            Effect.andThen(WebSocketBroadcastService, (wsService) => wsService.addConnection(ws)),
+            Effect.flatMap(WebSocketBroadcastService, (wsService) => wsService.addConnection(ws)),
           );
         },
         onClose: (_event, ws) => {
           EndpointRuntime.runSync(
-            Effect.andThen(WebSocketBroadcastService, (wsService) => wsService.removeConnection(ws)),
+            Effect.flatMap(WebSocketBroadcastService, (wsService) => wsService.removeConnection(ws)),
           );
         },
       };
